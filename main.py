56d1831f9e3e6d72055b5c92f85ec4d582cb10f4
Phil <pmash2@users.noreply.github.com>
2021-09-14 22:23:14 -0400

    Add logging to project
    https://docs.python.org/3/library/logging.html

---
+++ main.py
@@ -0,0 +1,17 @@
+import logging
+
+logger = logging.getLogger('main.logger')
+logger.setLevel('DEBUG')
+
+file_log_handler = logging.FileHandler('logfile.log')
+logger.addHandler(file_log_handler)
+
+stderr_log_handler = logging.StreamHandler()
+logger.addHandler(stderr_log_handler)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_log_handler.setFormatter(formatter)
+stderr_log_handler.setFormatter(formatter)
+
+logger.info('Info message')
+logger.error('Error message')
